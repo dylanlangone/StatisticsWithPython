0f6082971edfd78c2b7d2f0bbb16bc2941c4b74b
Dylan Langone <dylanlangone628@gmail.com>
2018-06-21 17:14:34 -0400

    Checked cases of paramters in binom() for validity

--- statutil.py
+++ statutil.py
@@ -14,6 +14,15 @@ def binom(x, n, p):
 	Gives probability of x times occurring for event of probability p
 	out of n trials
 	"""
+	if (p > 1 or p < 0):
+		raise ValueError("Enter a probability from 0 to 1")
+	
+	if (x < 0):
+		raise ValueError("Enter a valid number of successes")
+		
+	if (x > n):
+		raise ValueError("Make sure number of successes is less than or equal to number of trials");
+		
 	return combinutil.choose(n, x)*p**x*(1-p)**(n-x)
 
 
